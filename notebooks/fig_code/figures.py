43c722a4fb6fecf90f20e46d8eda46466890e649
Justin Lyons <lyonjust@outlook.com>
2022-07-22 23:13:02 +1000

    Update to latest sklearn API, matplotlib oop interface

--- notebooks/fig_code/figures.py
+++ notebooks/fig_code/figures.py
@@ -4,7 +4,8 @@ import warnings
 
 
 def plot_venn_diagram():
-    fig, ax = plt.subplots(subplot_kw=dict(frameon=False, xticks=[], yticks=[]))
+    fig, ax = plt.subplots(subplot_kw=dict(
+        frameon=False, xticks=[], yticks=[]))
     ax.add_patch(plt.Circle((0.3, 0.3), 0.3, fc='red', alpha=0.5))
     ax.add_patch(plt.Circle((0.6, 0.3), 0.3, fc='blue', alpha=0.5))
     ax.add_patch(plt.Rectangle((-0.1, -0.1), 1.1, 0.8, fc='none', ec='black'))
@@ -51,7 +52,7 @@ def plot_example_decision_tree():
     ax.axis([0, 1, 0, 1])
 
 
-def visualize_tree(estimator, X, y, boundaries=True,
+def visualize_tree(estimator, X, y, ax: plt.Axes, boundaries=True,
                    xlim=None, ylim=None):
     estimator.fit(X, y)
 
@@ -68,69 +69,68 @@ def visualize_tree(estimator, X, y, boundaries=True,
 
     # Put the result into a color plot
     Z = Z.reshape(xx.shape)
-    plt.figure()
-    plt.pcolormesh(xx, yy, Z, alpha=0.2, cmap='rainbow')
-    plt.clim(y.min(), y.max())
+    #plt.pcolormesh(xx, yy, Z, alpha=0.2, cmap='rainbow')
 
-    # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='rainbow')
-    plt.axis('off')
+    plt.rcParams['axes.grid'] = False
 
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)        
-    plt.clim(y.min(), y.max())
-    
+    ax.pcolormesh(xx, yy, Z, alpha=0.2, cmap='rainbow')
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=50,
+               cmap='rainbow', vmin=y.min(), vmax=y.max())
+    ax.axis('off')
+    ax.set_xlim(xmin=x_min, xmax=x_max)
+    ax.set_ylim(ymin=y_min, ymax=y_max)
+
     # Plot the decision boundaries
     def plot_boundaries(i, xlim, ylim):
         if i < 0:
             return
 
         tree = estimator.tree_
-        
+
         if tree.feature[i] == 0:
-            plt.plot([tree.threshold[i], tree.threshold[i]], ylim, '-k')
+            ax.plot([tree.threshold[i], tree.threshold[i]], ylim, '-k')
             plot_boundaries(tree.children_left[i],
                             [xlim[0], tree.threshold[i]], ylim)
             plot_boundaries(tree.children_right[i],
                             [tree.threshold[i], xlim[1]], ylim)
-        
+
         elif tree.feature[i] == 1:
-            plt.plot(xlim, [tree.threshold[i], tree.threshold[i]], '-k')
+            ax.plot(xlim, [tree.threshold[i], tree.threshold[i]], '-k')
             plot_boundaries(tree.children_left[i], xlim,
                             [ylim[0], tree.threshold[i]])
             plot_boundaries(tree.children_right[i], xlim,
                             [tree.threshold[i], ylim[1]])
-            
+
     if boundaries:
         plot_boundaries(0, plt.xlim(), plt.ylim())
 
 
-def plot_tree_interactive(X, y):
+def plot_tree_interactive(X, y, ax=None):
     from sklearn.tree import DecisionTreeClassifier
 
     def interactive_tree(depth=1):
         clf = DecisionTreeClassifier(max_depth=depth, random_state=0)
-        visualize_tree(clf, X, y)
+        fig, ax = plt.subplots()
+        visualize_tree(clf, X, y, ax=ax)
 
     from ipywidgets import interact
     return interact(interactive_tree, depth=(1, 5))
 
 
-def plot_kmeans_interactive(min_clusters=1, max_clusters=6):
+def plot_kmeans_interactive(min_clusters=2, max_clusters=6):
     from ipywidgets import interact
     from sklearn.metrics.pairwise import euclidean_distances
-    from sklearn.datasets.samples_generator import make_blobs
+    from sklearn.datasets import make_blobs
 
     with warnings.catch_warnings():
-        warnings.filterwarnings('ignore')
 
         X, y = make_blobs(n_samples=300, centers=4,
                           random_state=0, cluster_std=0.60)
 
         def _kmeans_step(frame=0, n_clusters=4):
-            rng = np.random.RandomState(2)
+            rng = np.random.default_rng(seed=2)
             labels = np.zeros(X.shape[0])
-            centers = rng.randn(n_clusters, 2)
+            centers = rng.standard_normal((n_clusters, 2))
 
             nsteps = frame // 3
 
@@ -141,57 +141,57 @@ def plot_kmeans_interactive(min_clusters=1, max_clusters=6):
                     labels = dist.argmin(1)
 
                 if i < nsteps or frame % 3 > 1:
-                    centers = np.array([X[labels == j].mean(0)
+                    # for the second cluster, no labels match... this causes mean of empty slice issue
+                    centers = np.array([X[labels == j].mean(0) if X[labels == j].any() else [np.nan] * 2
                                         for j in range(n_clusters)])
                     nans = np.isnan(centers)
                     centers[nans] = old_centers[nans]
 
-
             # plot the data and cluster centers
-            plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='rainbow',
-                        vmin=0, vmax=n_clusters - 1);
-            plt.scatter(old_centers[:, 0], old_centers[:, 1], marker='o',
-                        c=np.arange(n_clusters),
-                        s=200, cmap='rainbow')
-            plt.scatter(old_centers[:, 0], old_centers[:, 1], marker='o',
-                        c='black', s=50)
+            fig, ax = plt.subplots()
+
+            ax.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='rainbow',
+                       vmin=0, vmax=n_clusters - 1)
+            ax.scatter(old_centers[:, 0], old_centers[:, 1], marker='o',
+                       c=np.arange(n_clusters),
+                       s=200, cmap='rainbow')
+            ax.scatter(old_centers[:, 0], old_centers[:, 1], marker='o',
+                       c='black', s=50)
 
             # plot new centers if third frame
             if frame % 3 == 2:
                 for i in range(n_clusters):
-                    plt.annotate('', centers[i], old_centers[i], 
-                                 arrowprops=dict(arrowstyle='->', linewidth=1))
-                plt.scatter(centers[:, 0], centers[:, 1], marker='o',
-                            c=np.arange(n_clusters),
-                            s=200, cmap='rainbow')
-                plt.scatter(centers[:, 0], centers[:, 1], marker='o',
-                            c='black', s=50)
+                    ax.annotate('', centers[i], old_centers[i],
+                                arrowprops=dict(arrowstyle='->', linewidth=1))
+                ax.scatter(centers[:, 0], centers[:, 1], marker='o',
+                           c=np.arange(n_clusters),
+                           s=200, cmap='rainbow')
+                ax.scatter(centers[:, 0], centers[:, 1], marker='o',
+                           c='black', s=50)
 
-            plt.xlim(-4, 4)
-            plt.ylim(-2, 10)
+            ax.set_xlim(xmin=-4, xmax=4)
+            ax.set_ylim(ymin=-2, ymax=10)
 
             if frame % 3 == 1:
-                plt.text(3.8, 9.5, "1. Reassign points to nearest centroid",
-                         ha='right', va='top', size=14)
+                ax.text(3.8, 9.5, "1. Reassign points to nearest centroid",
+                        ha='right', va='top', size=14)
             elif frame % 3 == 2:
-                plt.text(3.8, 9.5, "2. Update centroids to cluster means",
-                         ha='right', va='top', size=14)
+                ax.text(3.8, 9.5, "2. Update centroids to cluster means",
+                        ha='right', va='top', size=14)
 
-    
     return interact(_kmeans_step, frame=(0, 50),
-                    n_clusters=[min_clusters, max_clusters])
+                    n_clusters=(min_clusters, max_clusters))
 
 
 def plot_image_components(x, coefficients=None, mean=0, components=None,
                           imshape=(8, 8), n_components=6, fontsize=12):
     if coefficients is None:
         coefficients = x
-        
+
     if components is None:
         components = np.eye(len(coefficients), len(x))
-        
+
     mean = np.zeros_like(x) + mean
-        
 
     fig = plt.figure(figsize=(1.2 * (5 + n_components), 1.2 * 2))
     g = plt.GridSpec(2, 5 + n_components, hspace=0.3)
@@ -229,5 +229,5 @@ def plot_pca_interactive(data, n_components=6):
     def show_decomp(i=0):
         plot_image_components(data[i], Xproj[i],
                               pca.mean_, pca.components_)
-    
-    interact(show_decomp, i=(0, data.shape[0] - 1));
+
+    interact(show_decomp, i=(0, data.shape[0] - 1))
--- notebooks/fig_code/helpers.py
+++ notebooks/fig_code/helpers.py
@@ -3,7 +3,8 @@ Small helpers for code that is not shown in the notebooks
 """
 
 from sklearn import neighbors, datasets, linear_model
-import pylab as pl
+#import pylab as pl
+import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
 
@@ -14,7 +15,7 @@ cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 def plot_iris_knn():
     iris = datasets.load_iris()
     X = iris.data[:, :2]  # we only take the first two features. We could
-                        # avoid this ugly slicing by using a two-dim dataset
+                    # avoid this ugly slicing by using a two-dim dataset
     y = iris.target
 
     knn = neighbors.KNeighborsClassifier(n_neighbors=3)
@@ -23,19 +24,19 @@ def plot_iris_knn():
     x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
     y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
-                         np.linspace(y_min, y_max, 100))
+                        np.linspace(y_min, y_max, 100))
     Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
 
     # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    pl.figure()
-    pl.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
+    Z = Z.reshape(xx.shape)
     # Plot also the training points
-    pl.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
-    pl.xlabel('sepal length (cm)')
-    pl.ylabel('sepal width (cm)')
-    pl.axis('tight')
+    fig, ax = plt.subplots()
+    ax.pcolormesh(xx, yy, Z, cmap=cmap_light, shading='auto')
+    ax.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+    ax.set_xlabel('sepal length (cm)')
+    ax.set_ylabel('sepal width (cm)')
+    fig.tight_layout()
 
 
 def plot_polynomial_regression():
